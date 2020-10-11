4d323a48da387f08cadb693cad4128ff622f2a84
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-10-11 16:43:55 -0400

    Adds testing helpers. Edits to other files

---
+++ KCPUtils/TestHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace KCPUtils.TestHelpers
+{
+    public class MockedFileSystem
+    {
+        public class Builder
+        {
+            private string currentDirectory;
+            private readonly Dictionary<string, MockFileData> fileContents = new Dictionary<string, MockFileData>();
+
+            public Builder() { }
+            public Builder(string currentDirectory) { this.currentDirectory = currentDirectory; }
+
+            public Builder AddFile(string filename, MockFileData contents)
+            {
+                this.fileContents.Add(filename, contents);
+                return this;
+            }
+
+            public Builder CurrentDirectory(string currentDirectory)
+            {
+                if (this.currentDirectory != null) { return this; }
+
+                this.currentDirectory = currentDirectory;
+                return this;
+            }
+
+            public IFileSystem Build()
+            {
+                return new MockFileSystem(this.fileContents, this.currentDirectory);
+            }
+        }
+    }
+}
